7461ea69528b2bca6fc068ce32690fe8eb42918b
AAAHAMED <asfakahamedamanulla@gmail.com>
2024-05-27 17:33:23 +0530

    "Updated Navbar component with new layout, added links and icons"

---
+++ client/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import KeyboardArrowDownRoundedIcon from '@mui/icons-material/KeyboardArrowDownRounded';
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import PersonOutlineRoundedIcon from '@mui/icons-material/PersonOutlineRounded';
+import FavoriteBorderRoundedIcon from '@mui/icons-material/FavoriteBorderRounded';
+import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded';
+
+
+export const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className="wrapper">
+         <div className="left"></div>
+            <div className="center"></div>
+            <div className="right"></div>
+        </div>
+    </div>
+  )
+}
